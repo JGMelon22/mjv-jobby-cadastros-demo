d53fdd79a9087fe539e4badd024558c1c262c3a6
JGMelon22 <jgvasconcellos22@gmail.com>
2023-07-04 17:38:35 -0300

    Desenvolvimento inicial da ProductController

---
+++ src/main/java/com/mjv/jobbycadastrosdemo/controllers/ProductController.java
@@ -0,0 +1,12 @@
+package com.mjv.jobbycadastrosdemo.controllers;
+
+import com.mjv.jobbycadastrosdemo.repositories.CadastroRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ProductController {
+
+    @Autowired
+    CadastroRepository cadastroRepository;
+}
